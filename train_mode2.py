a46ec795adc4d841c81887ba87e992c47de412da
Dui.NK <DUY.NK193292@sis.hust.edu.vn>
2022-08-06 18:31:21 +0700

    first commit

---
+++ train_mode2.py
@@ -0,0 +1,181 @@
+import os
+import warnings
+import cv2
+import keras
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+from keras import models, layers, optimizers
+from keras.applications.vgg16 import VGG16
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from keras.layers import Dense, Dropout, Flatten
+from keras.models import Model
+from keras.preprocessing import image as image_utils
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import to_categorical
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+#vggmodel = VGG16(weights='imagenet', include_top=True)
+
+# Dinh nghia cac bien
+
+gestures = {'Aa': 'A', 'Bb': 'B', 'Cc':'C', 'Dd':'D', 'Ee':'E', 'Ff':'F', 'Gg':'G', 'Hh':'H',
+           'Ia': 'I', 'Jj':'J', 'Ll':'L', 'Mm':'M', 'Nn':'N', 'Oo':'O', 'Pp':'P',
+           'So': 'S', 'Tt':'T', 'Uu':'U', 'Vv':'V', 'Yy':'Y', 'Zz':'Z',
+           'Rb': 'R',
+           'Qu': 'Q',
+            'We': 'W',
+            'Xi': 'X'
+            }
+
+gestures_map = {'A': 0,
+                'B': 1,
+                'C': 2,
+                'D': 3,
+                'E': 4,
+                'F': 5,
+                'G': 6,
+                'H': 7,
+                'I': 8,
+                'J': 9,
+                'L': 10,
+                'M': 11,
+                'N': 12,
+                'O': 13,
+                'P': 14,
+                'Q': 15,
+                'R': 16,
+                'S': 17,
+                'T': 18,
+                'U': 19,
+                'V': 20,
+                'W': 21,
+                'X': 22,
+                'Y': 23,
+                'Z': 24}
+
+
+gesture_names = {0: 'A',
+                 1: 'B',
+                 2: 'C',
+                 3: 'D',
+                 4: 'E',
+                 5: 'F',
+                 6: 'G',
+                 7: 'H',
+                 8: 'I',
+                 9: 'J',
+                 10: 'L',
+                 11: 'M',
+                 12: 'N',
+                 13: 'O',
+                 14: 'P',
+                 15: 'Q',
+                 16: 'R',
+                 17: 'S',
+                 18: 'T',
+                 19: 'U',
+                 20: 'V',
+                 21: 'W',
+                 22: 'X',
+                 23: 'Y',
+                 24: 'Z'}
+
+
+image_path = '/content/drive/MyDrive/datatest'
+models_path = '/content/MiAI_Hand_Lang/model/saved_model2.hdf5'
+rgb = False
+imageSize = 112
+
+
+# Ham xu ly anh resize ve 224x224 va chuyen ve numpy array
+def process_image(path):
+    img = Image.open(path)
+    img = img.resize((imageSize, imageSize))
+    img = np.array(img)
+    return img
+
+# Xu ly du lieu dau vao
+def process_data(X_data, y_data):
+    X_data = np.array(X_data, dtype = 'float32')
+    if rgb:
+        pass
+    else:
+        X_data = np.stack((X_data,)*3, axis=-1)
+    X_data /= 255
+    y_data = np.array(y_data)
+    y_data = to_categorical(y_data)
+    return X_data, y_data
+
+# Ham duuyet thu muc anh dung de train
+def walk_file_tree(image_path):
+    X_data = []
+    y_data = []
+    for directory, subdirectories, files in os.walk(image_path):
+        for file in files:
+            if not file.startswith('.'):
+                path = os.path.join(directory, file)
+                gesture_name = gestures[file[0:2]]
+                print(gesture_name)
+                print(gestures_map[gesture_name])
+                y_data.append(gestures_map[gesture_name])
+                X_data.append(process_image(path))
+
+            else:
+                continue
+
+    X_data, y_data = process_data(X_data, y_data)
+    return X_data, y_data
+
+# Load du lieu vao X va Y
+X_data, y_data = walk_file_tree(image_path)
+
+# Phan chia du lieu train va test theo ty le 80/20
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size = 0.2, random_state=12, stratify=y_data)
+
+# Dat cac checkpoint de luu lai model tot nhat
+model_checkpoint = ModelCheckpoint(filepath=models_path, save_best_only=True)
+early_stopping = EarlyStopping(monitor='val_acc',
+                               min_delta=0,
+                               patience=10,
+                               verbose=1,
+                               mode='auto',
+                               restore_best_weights=True)
+
+# Khoi tao model
+model1 = VGG16(weights='imagenet', include_top=False, input_shape=(imageSize, imageSize, 3))
+optimizer1 = tf.keras.optimizers.Adam()
+base_model = model1
+
+# Them cac lop ben tren
+x = base_model.output
+x = Flatten()(x)
+x = Dense(128, activation='relu', name='fc1')(x)
+x = Dense(128, activation='relu', name='fc2')(x)
+x = Dense(128, activation='relu', name='fc2a')(x)
+x = Dense(128, activation='relu', name='fc3')(x)
+x = Dropout(0.5)(x)
+x = Dense(64, activation='relu', name='fc4')(x)
+
+predictions = Dense(25, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Dong bang cac lop duoi, chi train lop ben tren minh them vao
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=50, batch_size=32, validation_data=(X_test, y_test), verbose=1,
+          callbacks=[early_stopping, model_checkpoint])
+
+# Luu model da train ra file
+model.save('/content/MiAI_Hand_Lang/model/mymodel2.h5')
+new_model = keras.models.load_model('/content/MiAI_Hand_Lang/model/mymodel2.h5')
+
+
